08445696ae425f121006610ba119c063e6ab9533
turkidot <56735962+turkidot@users.noreply.github.com>
2019-12-27 22:03:28 +0600

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,12 @@
+<?php
+$db['hostname'] = "localhost";
+$db['user'] = "root";
+$db['password']="";
+$db ['database'] = "delivery";
+$con = mysqli_connect ($db['hostname'],$db['user'],$db['password'],$db['database']);
+if (mysqli_connect_errno()) {
+   echo "Failed to connect to MYSQL: " . mysqli_connect_error();
+}
+else{
+    echo "connected";
+}
