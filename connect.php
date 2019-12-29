f33fd359471cd992e9547a070f541c1e56873049
turkidot <56735962+turkidot@users.noreply.github.com>
2019-12-28 21:38:22 -0800

    Update connect.php

--- connect.php
+++ connect.php
@@ -2,7 +2,7 @@
 $db['hostname'] = "localhost";
 $db['user'] = "root";
 $db['password']="";
-$db ['database'] = "delivery";
+$db ['database'] = "userlist";
 $con = mysqli_connect ($db['hostname'],$db['user'],$db['password'],$db['database']);
 if (mysqli_connect_errno()) {
    echo "Failed to connect to MYSQL: " . mysqli_connect_error();
